4665a5738dfd54036b6820cfbd537fef3dcdd590
Seagull404 <cn13635706133@gmail.com>
2025-10-09 22:28:36 +0800

    NLog

---
+++ LoggingDemo1/LoggingDemo1/Test1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggingDemo1
+{
+     class Test1
+    {
+        private readonly ILogger<Test1> _logger;
+        public Test1(ILogger<Test1> logger)
+        {
+            _logger = logger;
+        }
+        public void Test()
+        {
+            _logger.LogDebug("开始执行数据库同步");
+            _logger.LogDebug("连接数据库成功");
+            _logger.LogWarning("查找数据失败，重试第一次");
+            // 模拟重试
+            _logger.LogWarning("查找数据失败，重试第二次");
+            _logger.LogError("查找数据失败，放弃");
+        }
+    }
+}
---
+++ LoggingDemo1/LoggingDemo1/Test2.cs
@@ -0,0 +1,28 @@
+﻿using LoggingDemo1;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemServices
+{
+    internal class Test2
+    {
+        private readonly ILogger<Test2> _logger;
+        public Test2(ILogger<Test2> logger)
+        {
+            _logger = logger;
+        }
+        public void Test()
+        {
+            _logger.LogDebug("开始执行数FTP同步");
+            _logger.LogDebug("连接FTP成功");
+            _logger.LogWarning("查找数据失败，重试第一次");
+            // 模拟重试
+            _logger.LogWarning("查找数据失败，重试第二次");
+            _logger.LogError("查找数据失败，放弃");
+        }
+    }
+}
